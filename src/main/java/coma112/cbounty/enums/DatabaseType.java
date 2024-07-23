2f7046feffc31ceffcb4d163b26416f60d220ac9
Bercyy <bertalankisivan@gmail.com>
2024-07-23 14:45:29 +0200

    Added Turkish language. Thanks to WinTone01 for the translation!

--- src/main/java/coma112/cbounty/enums/DatabaseType.java
+++ src/main/java/coma112/cbounty/enums/DatabaseType.java
@@ -2,6 +2,5 @@ package coma112.cbounty.enums;
 
 public enum DatabaseType {
     MYSQL, mysql,
-    MONGODB, mongodb,
     SQLITE, sqlite
 }
--- src/main/java/coma112/cbounty/enums/LanguageType.java
+++ src/main/java/coma112/cbounty/enums/LanguageType.java
@@ -3,5 +3,6 @@ package coma112.cbounty.enums;
 public enum LanguageType {
     HU, hu,
     DE, de,
-    EN, en
+    EN, en,
+    TR, tr
 }
